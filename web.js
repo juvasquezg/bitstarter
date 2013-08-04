9ad0920e96620c020654d357020251e013869bf0
juvasquezg <juvasquez88@gmail.com>
2013-08-03 21:49:46 -0500

    Bug fs is not defined

--- web.js
+++ web.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
+var fs = require('fs');
 var infile = "index.html";
 var textBuffer = fs.readFileSync(infile);
 

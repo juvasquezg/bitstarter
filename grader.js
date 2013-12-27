2a22cd6d1282d4d0faae36f773b5711ab69f6b16
juvasquezg <juvasquez88@gmail.com>
2013-12-26 19:58:31 -0500

    update grader.js from juvasquezg home

--- grader.js
+++ grader.js
@@ -22,16 +22,18 @@ References:
 */
 
 var fs = require('fs');
+var rest = require('restler');
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
+var HEROKUURL = "http://agile-chamber-8377.herokuapp.com";
 var CHECKSFILE_DEFAULT = "checks.json";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
         console.log("%s does not exist. Exiting.", instr);
-        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+        //process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
     }
     return instr;
 };
@@ -61,14 +63,23 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+//rest.get(herokuurl).on('complete', function(result) {
+//    if (result instanceof Error) {
+//            console.error('Error: ' + util.format(response.message));
+//    }else{
+//	console.log(result);
+//    }
+//});
+
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url_path>', 'path to url index.html', clone(assertFileExists), HEROKUURL)
         .parse(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {
     exports.checkHtmlFile = checkHtmlFile;
-}
+};
